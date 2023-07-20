2dc79b5cbaabb8c939d5851f163be9e6050a387e
PjotrVr <stika.petar@gmail.com>
2023-07-20 20:39:41 +0200

    Add functionality to downscale video to 360p

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,13 +1,31 @@
 import express from "express";
+import ffmpeg from "fluent-ffmpeg";
+
 
 const app = express();
-const port = 3000;
+app.use(express.json()) // use json
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+app.post("/process-video", (req, res) => {
+    // get path of the input video file from the req body
+    const inputFilePath = req.body.inputFilePath;
+    const outputFilePath = req.body.outputFilePath;
+
+   if (!inputFilePath || !outputFilePath) {
+    res.status(400).send("Bad request: Missing file path.");
+   } 
+
+   ffmpeg(inputFilePath)
+        .outputOptions("-vf", "scale=-1:360") // converts to 360p
+        .on("end", () => {
+            res.status(200).send("Video processing finished successfully.");
+        }).on("error", (err) => {
+            console.log(`An error occurred: ${err.message}`);
+            res.status(500).send(`Internal Server Error: ${err.message}`)
+        })
+        .save(outputFilePath);
 });
 
-app.listen(port, () => {
-    console.log(`Video processing service listening at http://localhost:${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Video processing service listening at http://localhost:${PORT}`);
 });
-
